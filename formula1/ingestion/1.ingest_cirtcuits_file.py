d2cba43100e7af55e44ada4e42fe0ee6972be7b0
maryletteroa <maryletteroa>
2021-09-15 01:55:43 +0000

    add schema to circuits file

--- formula1/ingestion/1.ingest_cirtcuits_file.py
+++ formula1/ingestion/1.ingest_cirtcuits_file.py
@@ -22,11 +22,49 @@ ls /mnt/formula1dlmr/raw
 # COMMAND ----------
 
 # read circuits.csv as a dataframe using DataFrame API
-# .option() enables additional arguments e.g. specifying that a header is present
-circuits_df = spark.read.option("header", True).csv("dbfs:/mnt/formula1dlmr/raw/circuits.csv")
+# .option() enables additional arguments
+# using two options result in two spark jobs resulting in performance limitations
+
+circuits_df = spark.read \
+.option("header", True) \
+.option("inferSchema", True) \
+.csv("dbfs:/mnt/formula1dlmr/raw/circuits.csv")
 
 # COMMAND ----------
 
+# for production level projects better to specify the schema manually
+# specially if you're loading large datasets
+
+from pyspark.sql.types import StructType,StructField, IntegerType, StringType, DoubleType
+
+# COMMAND ----------
+
+# StructType specifies rows
+# StructField species columns
+
+circuits_schema = StructType(fields = [StructField("circuitId", IntegerType(), False),
+  StructField("circuitRef", StringType(), True),
+  StructField("name", StringType(), True),
+  StructField("location", StringType(), True),
+  StructField("country", StringType(), True),
+  StructField("lat", DoubleType(), True),
+  StructField("lng", DoubleType(), True),
+  StructField("alt", IntegerType(), True),
+  StructField("url", StringType(), True),
+])
+
+# COMMAND ----------
+
+# specify the schema
+
+circuits_df = spark.read \
+.option("header", True) \
+.schema(circuits_schema) \
+.csv("dbfs:/mnt/formula1dlmr/raw/circuits.csv")
+
+# COMMAND ----------
+
+# determine data type of objects
 type(circuits_df)
 
 # COMMAND ----------
@@ -37,3 +75,11 @@ circuits_df.show()
 # COMMAND ----------
 
 display(circuits_df)
+
+# COMMAND ----------
+
+circuits_df.printSchema()
+
+# COMMAND ----------
+
+circuits_df.describe().show()
