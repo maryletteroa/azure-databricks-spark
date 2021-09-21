17649722352c8754fdd9b2f1cf5bbc21e1065906
maryletteroa <maryletteroa>
2021-09-21 06:52:26 +0000

    add join demo semi anti cross

--- formula1/demo/2.join_demo.py
+++ formula1/demo/2.join_demo.py
@@ -53,4 +53,45 @@ race_circuits_df = circuits_df.join(races_df, circuits_df.circuit_id == races_df
 
 # COMMAND ----------
 
+# MAGIC %md
+# MAGIC #### Semi Joins
+
+# COMMAND ----------
+
+# semi joins only allows access of columns at the left side of the join
+# hence no access to races_df.race_name, and races_df.round, must be removed
+# similar to inner joins and selecting only columns from left dataframe
+
+race_circuits_df = circuits_df.join(races_df, circuits_df.circuit_id == races_df.circuit_id, "semi")
+
+# COMMAND ----------
+
+# MAGIC %md
+# MAGIC #### Anti Joins
+
+# COMMAND ----------
+
+# anything on the left dataframe not found in the right dataframe
+
+race_circuits_df = races_df.join(circuits_df, circuits_df.circuit_id == races_df.circuit_id, "anti")
+
+# COMMAND ----------
+
+# MAGIC %md
+# MAGIC #### Cross Joins
+
+# COMMAND ----------
+
+# gives  the cartesian product of the two dataframes
+# int(races_df.count()) * int(circuits_df.count())
+
+race_circuits_df = races_df.crossJoin(circuits_df)
+
+# COMMAND ----------
+
+#
+
+# COMMAND ----------
+
 display(race_circuits_df)
+# race_circuits_df.count()
