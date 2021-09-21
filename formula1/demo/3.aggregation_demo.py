a82e727d63dfad0cc9dbf6585750742d810a836e
maryletteroa <maryletteroa>
2021-09-21 08:36:16 +0000

    add window functions

--- formula1/demo/3.aggregation_demo.py
+++ formula1/demo/3.aggregation_demo.py
@@ -82,3 +82,38 @@ demo_df \
 .groupBy("driver_name") \
 .agg(sum("points").alias("total_points"), countDistinct("race_name").alias("number_of_races")) \
 .show()
+
+# COMMAND ----------
+
+# MAGIC %md
+# MAGIC #### Window Functions
+
+# COMMAND ----------
+
+demo_df = race_results_df.filter("race_year in (2019, 2020)")
+
+# COMMAND ----------
+
+demo_grouped_df = demo_df \
+.groupBy("race_year", "driver_name") \
+.agg(sum("points").alias("total_points"), countDistinct("race_name").alias("number_of_races"))
+
+# COMMAND ----------
+
+display(demo_grouped_df)
+
+# COMMAND ----------
+
+from pyspark.sql.window import Window
+from pyspark.sql.functions import desc, rank
+
+# specifications
+driverRankSpec = Window.partitionBy("race_year").orderBy(desc("total_points"))
+
+# apply to dataframe
+# ranks are refreshed by race_year
+# rank is a Window function
+# orderby and partitionby are WindowSpecs function
+
+
+demo_grouped_df.withColumn("rank", rank().over(driverRankSpec)).show(100)
