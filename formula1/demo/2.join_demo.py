a09bf11fc99e58c57af16be61d1f8b6a622dc37b
maryletteroa <maryletteroa>
2021-09-21 06:40:12 +0000

    add join demo outer

--- formula1/demo/2.join_demo.py
+++ formula1/demo/2.join_demo.py
@@ -7,6 +7,7 @@
 # between the two databases
 
 circuits_df = spark.read.parquet(f"{processed_folder_path}/circuits") \
+.filter("circuit_id < 70") \
 .withColumnRenamed("name", "circuit_name")
 
 # COMMAND ----------
@@ -16,7 +17,38 @@ races_df = spark.read.parquet(f"{processed_folder_path}/races").filter("race_yea
 
 # COMMAND ----------
 
+# MAGIC %md
+# MAGIC #### Inner Joins
+
+# COMMAND ----------
+
 race_circuits_df = circuits_df.join(races_df, circuits_df.circuit_id == races_df.circuit_id, "inner") \
+.select(circuits_df.circuit_name, circuits_df.location, circuits_df.country, races_df.race_name, races_df.round)
+
+# COMMAND ----------
+
+# MAGIC %md
+# MAGIC #### Outer Joins
+
+# COMMAND ----------
+
+# Left Outer Join
+
+race_circuits_df = circuits_df.join(races_df, circuits_df.circuit_id == races_df.circuit_id, "left") \
+.select(circuits_df.circuit_name, circuits_df.location, circuits_df.country, races_df.race_name, races_df.round)
+
+# COMMAND ----------
+
+# Right Outer Join
+
+race_circuits_df = circuits_df.join(races_df, circuits_df.circuit_id == races_df.circuit_id, "right") \
+.select(circuits_df.circuit_name, circuits_df.location, circuits_df.country, races_df.race_name, races_df.round)
+
+# COMMAND ----------
+
+# Full Outer Join
+
+race_circuits_df = circuits_df.join(races_df, circuits_df.circuit_id == races_df.circuit_id, "full") \
 .select(circuits_df.circuit_name, circuits_df.location, circuits_df.country, races_df.race_name, races_df.round)
 
 # COMMAND ----------
