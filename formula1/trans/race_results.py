58fa8031d657ae91844f96b5aff4414b240c6c7d
maryletteroa <maryletteroa>
2021-09-21 08:12:04 +0000

    add trasnform to race_results

---
+++ formula1/trans/race_results.py
@@ -0,0 +1,68 @@
+# Databricks notebook source
+# MAGIC %run "../includes/configuration"
+
+# COMMAND ----------
+
+# MAGIC %run "../includes/common_functions"
+
+# COMMAND ----------
+
+races_df = spark.read.parquet(f"{processed_folder_path}/races") \
+.withColumnRenamed("name", "race_name") \
+.withColumnRenamed("race_timestamp", "race_date") \
+.select("race_id", "circuit_id", "race_year", "race_name", "race_date")
+
+# COMMAND ----------
+
+circuits_df = spark.read.parquet(f"{processed_folder_path}/circuits") \
+.withColumnRenamed("location", "circuit_location") \
+.select("circuit_id", "circuit_location")
+
+# COMMAND ----------
+
+drivers_df = spark.read.parquet(f"{processed_folder_path}/drivers") \
+.withColumnRenamed("name", "driver_name") \
+.withColumnRenamed("number", "driver_number") \
+.withColumnRenamed("nationality", "driver_nationality") \
+.select("driver_id", "driver_name", "driver_number", "driver_nationality")
+
+# COMMAND ----------
+
+constructors_df = spark.read.parquet(f"{processed_folder_path}/constructors") \
+.withColumnRenamed("name", "team") \
+.select("constructor_id", "team")
+
+# COMMAND ----------
+
+results_df = spark.read.parquet(f"{processed_folder_path}/results") \
+.withColumnRenamed("time", "race_time") \
+.select("result_id", "race_id", "driver_id", "constructor_id", "grid", "fastest_lap", "race_time", "points")
+
+# COMMAND ----------
+
+circuits_races_df = circuits_df.join(races_df, circuits_df.circuit_id == races_df.circuit_id)
+
+# COMMAND ----------
+
+final_df = results_df.join(circuits_races_df, results_df.race_id == circuits_races_df.race_id) \
+        .join(drivers_df, results_df.driver_id == drivers_df.driver_id) \
+        .join(constructors_df, results_df.constructor_id == constructors_df.constructor_id)
+
+# COMMAND ----------
+
+final_df = add_ingestion_date(final_df) \
+.withColumnRenamed("ingestion_date", "created_date") \
+.drop("constructor_id", "driver_id", "circuit_id", "race_id", "result_id")
+
+# COMMAND ----------
+
+# re-arrange columns according to requirements
+final_df = final_df.select("race_year", "race_name", "race_date", "circuit_location", "driver_name", "driver_number", "driver_nationality", "team", "grid", "fastest_lap", "race_time", "points", "created_date")
+
+# COMMAND ----------
+
+display(final_df.filter("race_year == 2020 and race_name == 'Abu Dhabi Grand Prix'").orderBy(final_df.points.desc()))
+
+# COMMAND ----------
+
+final_df.write.mode("overwrite").parquet(f"{presentation_folder_path}/race_results")
