8a76529fd5fddd894d91444874a184c0efa14b76
maryletteroa <maryletteroa>
2021-09-22 06:33:05 +0000

    add views on sql tables demo

--- formula1/demo/6.sql_objects_demo.sql
+++ formula1/demo/6.sql_objects_demo.sql
@@ -241,3 +241,74 @@ LOCATION "/mnt/formula1dlmr/presentation/race_resuls_ext_sql"
 
 -- specify LOCATION for external tables
 -- just created table, no data yet
+
+-- COMMAND ----------
+
+-- MAGIC %md
+-- MAGIC ### Views on tables
+-- MAGIC 
+-- MAGIC #### Learning Objectives
+-- MAGIC 1. Create Temp View
+-- MAGIC 2. Create Global Temp View
+-- MAGIC 3. Create Permanent View
+
+-- COMMAND ----------
+
+SELECT CURRENT_DATABASE();
+
+-- COMMAND ----------
+
+CREATE OR REPLACE TEMP VIEW v_race_results
+AS
+SELECT * 
+  FROM demo.race_results_python
+WHERE race_year = 2018;
+
+-- COMMAND ----------
+
+SELECT * from v_race_results;
+
+-- COMMAND ----------
+
+CREATE OR REPLACE GLOBAL TEMP VIEW gv_race_results
+AS
+SELECT * 
+  FROM demo.race_results_python
+WHERE race_year = 2012;
+
+-- COMMAND ----------
+
+SELECT * FROM global_temp.gv_race_results
+
+-- COMMAND ----------
+
+SHOW TABLES IN global_temp;
+
+-- COMMAND ----------
+
+-- for views that  you want to be able to come back and access
+-- example summarized data for dashboards
+
+USE default;
+CREATE OR REPLACE VIEW pv_race_results
+AS
+SELECT * 
+  FROM demo.race_results_python
+WHERE race_year = 2000;
+
+-- COMMAND ----------
+
+CREATE OR REPLACE VIEW demo.pv_race_results
+AS
+SELECT * 
+  FROM demo.race_results_python
+WHERE race_year = 2000;
+
+-- COMMAND ----------
+
+SHOW TABLES IN demo;
+
+-- COMMAND ----------
+
+SELECT * FROM demo.pv_race_results
+
