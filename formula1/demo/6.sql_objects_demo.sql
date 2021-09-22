b70bd5a48a571ba8cd779f0c633814c28187b035
maryletteroa <maryletteroa>
2021-09-22 06:04:03 +0000

    add managed tables

--- formula1/demo/6.sql_objects_demo.sql
+++ formula1/demo/6.sql_objects_demo.sql
@@ -67,3 +67,74 @@ SHOW TABLES;
 -- COMMAND ----------
 
 SHOW TABLES in default;
+
+-- COMMAND ----------
+
+-- MAGIC %md
+-- MAGIC ### Learning Objectives
+-- MAGIC 1. Create managed table using Python
+-- MAGIC 2. Create managed table using SQL
+-- MAGIC 3. Effect of dropping a managed table
+-- MAGIC 4. Describe table
+
+-- COMMAND ----------
+
+-- MAGIC %run "../includes/configuration"
+
+-- COMMAND ----------
+
+-- MAGIC %python
+-- MAGIC race_results_df = spark.read.parquet(f"{presentation_folder_path}/race_results")
+
+-- COMMAND ----------
+
+-- MAGIC %python 
+-- MAGIC 
+-- MAGIC # creates a managed table
+-- MAGIC # since file name has been specified without a path
+-- MAGIC race_results_df.write.format("parquet").saveAsTable("demo.race_results_python")
+-- MAGIC 
+-- MAGIC # this table is now in Data
+
+-- COMMAND ----------
+
+USE demo;
+SHOW TABLES;
+
+-- COMMAND ----------
+
+DESC EXTENDED race_results_python;
+
+-- COMMAND ----------
+
+-- qualify column with the table name just so it's clear what db you're getting the data from
+
+SELECT * 
+  FROM demo.race_results_python
+WHERE race_year = 2020;
+
+-- COMMAND ----------
+
+-- create managed table using SQL
+
+CREATE TABLE demo.race_results_sql
+AS
+SELECT * 
+  FROM demo.race_results_python
+WHERE race_year = 2020;
+
+-- COMMAND ----------
+
+DESC EXTENDED demo.race_results_sql;
+
+-- COMMAND ----------
+
+DROP TABLE demo.race_results_sql;
+
+-- COMMAND ----------
+
+SHOW TABLES IN demo;
+
+-- COMMAND ----------
+
+-- because it's a managed table, both data and metadata has been deleted
