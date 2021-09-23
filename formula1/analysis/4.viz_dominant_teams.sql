a51e7d632bf6f7dd49742f3e3ba47ee7013f34a6
maryletteroa <maryletteroa>
2021-09-23 05:51:26 +0000

    add dashboard for dominant teams

--- formula1/analysis/4.viz_dominant_teams.sql
+++ formula1/analysis/4.viz_dominant_teams.sql
@@ -1,4 +1,10 @@
 -- Databricks notebook source
+-- MAGIC %python
+-- MAGIC html = """<h1 style="color:Black;text-align:center;font-family:Ariel">Report on Dominant Formula 1 Teams </h1>"""
+-- MAGIC displayHTML(html)
+
+-- COMMAND ----------
+
 SELECT team_name,
         COUNT(1) AS total_races,
         SUM(calculated_points) AS total_points,
