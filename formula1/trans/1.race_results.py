ea2dcb0e0e3632b68c4cdb2c5910d889b4e790b9
maryletteroa <maryletteroa>
2021-09-23 12:35:03 +0000

    load race_results incrementaly

--- formula1/trans/1.race_results.py
+++ formula1/trans/1.race_results.py
@@ -1,4 +1,9 @@
 # Databricks notebook source
+dbutils.widgets.text("p_file_date", "2021-03-21")
+v_file_date = dbutils.widgets.get("p_file_date")
+
+# COMMAND ----------
+
 # MAGIC %run "../includes/configuration"
 
 # COMMAND ----------
@@ -34,9 +39,14 @@ constructors_df = spark.read.parquet(f"{processed_folder_path}/constructors") \
 
 # COMMAND ----------
 
+# results is incremental load so need to process the new date
+# add filter(...)
+
 results_df = spark.read.parquet(f"{processed_folder_path}/results") \
+.filter(f"file_date = '{v_file_date}'") \
 .withColumnRenamed("time", "race_time") \
-.select("result_id", "race_id", "driver_id", "constructor_id", "grid", "fastest_lap", "race_time", "points", "position")
+.withColumnRenamed("race_id", "result_race_id") \
+.select("result_id", "result_race_id", "driver_id", "constructor_id", "grid", "fastest_lap", "race_time", "points", "position")
 
 # COMMAND ----------
 
@@ -44,7 +54,7 @@ circuits_races_df = circuits_df.join(races_df, circuits_df.circuit_id == races_d
 
 # COMMAND ----------
 
-final_df = results_df.join(circuits_races_df, results_df.race_id == circuits_races_df.race_id) \
+final_df = results_df.join(circuits_races_df, results_df.result_race_id == circuits_races_df.race_id) \
         .join(drivers_df, results_df.driver_id == drivers_df.driver_id) \
         .join(constructors_df, results_df.constructor_id == constructors_df.constructor_id)
 
@@ -52,24 +62,28 @@ final_df = results_df.join(circuits_races_df, results_df.race_id == circuits_rac
 
 final_df = add_ingestion_date(final_df) \
 .withColumnRenamed("ingestion_date", "created_date") \
-.drop("constructor_id", "driver_id", "circuit_id", "race_id", "result_id")
+.drop("constructor_id", "driver_id", "circuit_id", "result_id")
 
 # COMMAND ----------
 
 # re-arrange columns according to requirements
-final_df = final_df.select("race_year", "race_name", "race_date", "circuit_location", "driver_name", "driver_number", "driver_nationality", "team", "grid", "fastest_lap", "race_time", "points", "position", "created_date")
-
-# COMMAND ----------
-
-display(final_df.filter("race_year == 2020 and race_name == 'Abu Dhabi Grand Prix'").orderBy(final_df.points.desc()))
+final_df = final_df.select("race_id", "race_year", "race_name", "race_date", "circuit_location", "driver_name", "driver_number", "driver_nationality", "team", "grid", "fastest_lap", "race_time", "points", "position", "created_date")
 
 # COMMAND ----------
 
 # final_df.write.mode("overwrite").parquet(f"{presentation_folder_path}/race_results")
-final_df.write.mode("overwrite").format("parquet").saveAsTable("f1_presentation.race_results")
+# final_df.write.mode("overwrite").format("parquet").saveAsTable("f1_presentation.race_results")
+overwrite_partition(final_df, "f1_presentation.race_results", "race_id")
 
 # COMMAND ----------
 
 # MAGIC %sql
 # MAGIC SELECT * FROM f1_presentation.race_results
-# MAGIC WHERE race_year = 1953;
+
+# COMMAND ----------
+
+# MAGIC %sql
+# MAGIC SELECT race_id, COUNT(1)
+# MAGIC FROM f1_presentation.race_results
+# MAGIC GROUP BY race_id
+# MAGIC ORDER BY race_id DESC
