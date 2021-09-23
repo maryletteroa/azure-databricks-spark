73e0329167beb194ebbcdd18e60e6f728c78e9a5
maryletteroa <maryletteroa>
2021-09-23 06:55:23 +0000

    ingest results file incrementally

--- formula1/ingestion/5.ingest_results_file.py
+++ formula1/ingestion/5.ingest_results_file.py
@@ -1,11 +1,20 @@
 # Databricks notebook source
 # MAGIC %md
-# MAGIC ### ingest results.json file
+# MAGIC ### Ingest results.json file
 
 # COMMAND ----------
 
 dbutils.widgets.text("p_data_source", "")
 v_data_source = dbutils.widgets.get("p_data_source")
+
+# COMMAND ----------
+
+dbutils.widgets.text("p_file_date", "2021-03-28")
+v_file_date = dbutils.widgets.get("p_file_date")
+
+# COMMAND ----------
+
+v_file_date
 
 # COMMAND ----------
 
@@ -50,7 +59,7 @@ results_schema = StructType(fields = [StructField("resultId", IntegerType(), Fal
 
 results_df = spark.read \
 .schema(results_schema) \
-.json(f"{raw_folder_path}/results.json")
+.json(f"{raw_folder_path}/{v_file_date}/results.json")
 
 # COMMAND ----------
 
@@ -73,7 +82,8 @@ results_final_df = results_df.withColumnRenamed("resultId", "result_id") \
     .withColumnRenamed("fastestLapTime", "fastest_lap_time") \
     .withColumnRenamed("fastestLapSpeed", "fastest_lap_speed") \
     .drop(col("statusId"))\
-    .withColumn("data_source", lit(v_data_source))
+    .withColumn("data_source", lit(v_data_source)) \
+    .withColumn("file_date", lit(v_file_date))
 
 # COMMAND ----------
 
@@ -86,10 +96,23 @@ results_final_df = add_ingestion_date(results_final_df)
 
 # COMMAND ----------
 
+# collects takes all the data and puts these in the driver node memory
+# use for small data not millions of rows
+
+for race_id_list in results_final_df.select("race_id").distinct().collect():
+  # check if table exists before dropping
+  # takes a while specialy for the checkout data since this loops through all race_ids
+  if (spark._jsparkSession.catalog().tableExists("f1_processed.results")): 
+    spark.sql(f"ALTER TABLE f1_processed.results DROP IF EXISTS PARTITION (race_id = {race_id_list.race_id})")
+
+# COMMAND ----------
+
 # results_final_df.write.mode("overwrite")\
 # .partitionBy("race_id") \
 # .parquet(f"{processed_folder_path}/results")
-results_final_df.write.mode("overwrite").format("parquet").saveAsTable("f1_processed.results")
+results_final_df.write.mode("append")\
+.partitionBy("race_id") \
+.format("parquet").saveAsTable("f1_processed.results")
 
 # COMMAND ----------
 
@@ -98,3 +121,11 @@ display(spark.read.parquet(f"{processed_folder_path}/results"))
 # COMMAND ----------
 
 dbutils.notebook.exit("Success")
+
+# COMMAND ----------
+
+# MAGIC %sql
+# MAGIC SELECT race_id, COUNT(1)
+# MAGIC FROM f1_processed.results
+# MAGIC GROUP BY race_id
+# MAGIC ORDER BY race_id DESC;
