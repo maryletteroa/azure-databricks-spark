a41166466fec7aceea70043d3007e7f596f7f331
maryletteroa <maryletteroa>
2021-09-24 02:10:16 +0000

    add incremental load to presentation tables

--- formula1/trans/2.driver_standings.py
+++ formula1/trans/2.driver_standings.py
@@ -1,13 +1,41 @@
 # Databricks notebook source
+dbutils.widgets.text("p_file_date", "2021-03-21")
+v_file_date = dbutils.widgets.get("p_file_date")
+
+# COMMAND ----------
+
 # MAGIC %run "../includes/configuration"
 
 # COMMAND ----------
 
-race_results_df = spark.read.parquet(f"{presentation_folder_path}/race_results")
+# MAGIC %run "../includes/common_functions"
 
 # COMMAND ----------
 
-display(race_results_df)
+# MAGIC %md
+# MAGIC Find race years for which the data is to be processed
+
+# COMMAND ----------
+
+# gives all the distinct race years
+race_results_list = spark.read.parquet(f"{presentation_folder_path}/race_results") \
+.filter(f"file_date = '{v_file_date}'") \
+.select("race_year")\
+.distinct() \
+.collect()
+
+# COMMAND ----------
+
+race_year_list = []
+for race_year in race_results_list:
+  race_year_list.append(race_year.race_year)
+
+# COMMAND ----------
+
+from pyspark.sql.functions import col
+
+race_results_df = spark.read.parquet(f"{presentation_folder_path}/race_results") \
+.filter(col("race_year").isin(race_year_list))
 
 # COMMAND ----------
 
@@ -21,10 +49,6 @@ driver_standings_df = race_results_df \
 
 # COMMAND ----------
 
-display(driver_standings_df.filter("race_year = 2020"))
-
-# COMMAND ----------
-
 from pyspark.sql.window import Window
 from pyspark.sql.functions import desc, rank
 
@@ -33,14 +57,20 @@ final_df = driver_standings_df.withColumn("rank", rank().over(driver_rank_spec))
 
 # COMMAND ----------
 
-display(final_df.filter("race_year = 2020"))
-
-# COMMAND ----------
-
 # final_df.write.mode("overwrite").parquet(f"{presentation_folder_path}/driver_standings")
-final_df.write.mode("overwrite").format("parquet").saveAsTable("f1_presentation.driver_standings")
+# final_df.write.mode("overwrite").format("parquet").saveAsTable("f1_presentation.driver_standings")
+overwrite_partition(final_df, "f1_presentation.driver_standings", "race_year")
 
 # COMMAND ----------
 
 # MAGIC %sql
-# MAGIC SELECT * FROM f1_presentation.driver_standings;
+# MAGIC SELECT *
+# MAGIC FROM f1_presentation.driver_standings;
+
+# COMMAND ----------
+
+# MAGIC %sql
+# MAGIC SELECT race_year, COUNT(1)
+# MAGIC FROM f1_presentation.driver_standings
+# MAGIC GROUP BY race_year
+# MAGIC ORDER BY race_year DESC
