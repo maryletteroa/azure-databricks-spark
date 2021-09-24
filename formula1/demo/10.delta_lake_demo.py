701da0f85e5b8ee2e78cef85ea1e9cf426b70535
maryletteroa <maryletteroa>
2021-09-24 07:03:19 +0000

    add delta lake demo file

--- formula1/demo/10.delta_lake_demo.py
+++ formula1/demo/10.delta_lake_demo.py
@@ -251,3 +251,195 @@ deltaTable.alias("tgt").merge(
 
 # MAGIC %sql
 # MAGIC SELECT * FROM f1_demo.drivers_merge;
+
+# COMMAND ----------
+
+# MAGIC %md
+# MAGIC 1. History & Versioning
+# MAGIC 2. Time Travel
+# MAGIC 3. Vaccuum
+
+# COMMAND ----------
+
+# MAGIC %sql
+# MAGIC DESC HISTORY f1_demo.drivers_merge
+
+# COMMAND ----------
+
+# MAGIC %sql
+# MAGIC SELECT * FROM f1_demo.drivers_merge VERSION AS OF 2;
+
+# COMMAND ----------
+
+# MAGIC %sql
+# MAGIC SELECT * FROM f1_demo.drivers_merge TIMESTAMP AS OF "2021-09-24T01:57:53.000+0000";
+
+# COMMAND ----------
+
+df = spark.read.format("delta").option("timeStampAsOf", "2021-09-24T01:57:53.000+0000").load("/mnt/formula1dlmr/demo/drivers_merge")
+
+# COMMAND ----------
+
+display(df)
+
+# COMMAND ----------
+
+# MAGIC %sql
+# MAGIC -- for gdpr purposes, use vaccumm to remove data from history
+# MAGIC -- default is remove data that are 7-days old or more, specify RETAIN
+# MAGIC 
+# MAGIC SET spark.databricks.delta.retentionDurationCheck.enabled = false;
+# MAGIC VACUUM f1_demo.drivers_merge RETAIN 0 HOURS
+
+# COMMAND ----------
+
+# MAGIC %sql
+# MAGIC SELECT * FROM f1_demo.drivers_merge TIMESTAMP AS OF "2021-09-24T01:57:53.000+0000";
+
+# COMMAND ----------
+
+# MAGIC %sql
+# MAGIC SELECT * FROM f1_demo.drivers_merge
+
+# COMMAND ----------
+
+# MAGIC %sql
+# MAGIC DESC HISTORY f1_demo.drivers_merge;
+
+# COMMAND ----------
+
+# MAGIC %sql
+# MAGIC DELETE FROM f1_demo.drivers_merge WHERE driverId = 1;
+
+# COMMAND ----------
+
+# MAGIC %sql
+# MAGIC SELECT * FROM f1_demo.drivers_merge VERSION AS OF 3;
+
+# COMMAND ----------
+
+# MAGIC %sql
+# MAGIC MERGE INTO f1_demo.drivers_merge tgt
+# MAGIC USING f1_demo.drivers_merge VERSION AS OF 3 src
+# MAGIC   ON (tgt.driverId = src.driverId)
+# MAGIC WHEN NOT MATCHED THEN
+# MAGIC   INSERT *
+
+# COMMAND ----------
+
+# MAGIC %sql
+# MAGIC DESC HISTORY f1_demo.drivers_merge;
+
+# COMMAND ----------
+
+# MAGIC %sql
+# MAGIC SELECT * FROM f1_demo.drivers_merge
+
+# COMMAND ----------
+
+# MAGIC %md
+# MAGIC Transaction Logs
+
+# COMMAND ----------
+
+# MAGIC %sql
+# MAGIC CREATE TABLE IF NOT EXISTS f1_demo.drivers_txn(
+# MAGIC driverId INT,
+# MAGIC dob DATE,
+# MAGIC forename STRING,
+# MAGIC surname STRING,
+# MAGIC createdDate DATE,
+# MAGIC updatedDate DATE
+# MAGIC )
+# MAGIC USING DELTA
+
+# COMMAND ----------
+
+# MAGIC %sql
+# MAGIC DESC HISTORY f1_demo.drivers_txn;
+
+# COMMAND ----------
+
+# MAGIC %sql
+# MAGIC INSERT INTO f1_demo.drivers_txn
+# MAGIC SELECT * FROM f1_demo.drivers_merge
+# MAGIC WHERE driverId = 1;
+
+# COMMAND ----------
+
+# MAGIC %sql
+# MAGIC DESC HISTORY f1_demo.drivers_txn;
+
+# COMMAND ----------
+
+# MAGIC %sql
+# MAGIC INSERT INTO f1_demo.drivers_txn
+# MAGIC SELECT * FROM f1_demo.drivers_merge
+# MAGIC WHERE driverId = 2;
+
+# COMMAND ----------
+
+# MAGIC %sql
+# MAGIC DELETE FROM f1_demo.drivers_txn
+# MAGIC WHERE driverId = 1;
+
+# COMMAND ----------
+
+for driver_id in range(3, 20):
+  spark.sql(f"""INSERT INTO f1_demo.drivers_txn
+    SELECT * FROM f1_demo.drivers_merge
+    WHERE driverId = {driver_id}""")
+
+# COMMAND ----------
+
+# MAGIC %sql
+# MAGIC INSERT INTO f1_demo.drivers_txn
+# MAGIC SELECT * FROM f1_demo.drivers_merge;
+# MAGIC 
+# MAGIC -- keep in mind to limit storing the transaction logs sice that might cost
+
+# COMMAND ----------
+
+# MAGIC %md
+# MAGIC Convert Parquet to Delta
+
+# COMMAND ----------
+
+# example when migrating projects
+
+# COMMAND ----------
+
+# MAGIC %sql
+# MAGIC CREATE TABLE IF NOT EXISTS f1_demo.drivers_convert_to_delta(
+# MAGIC driverId INT,
+# MAGIC dob DATE,
+# MAGIC forename STRING,
+# MAGIC surname STRING,
+# MAGIC createdDate DATE,
+# MAGIC updatedDate DATE
+# MAGIC )
+# MAGIC USING PARQUET
+
+# COMMAND ----------
+
+# MAGIC %sql
+# MAGIC INSERT INTO f1_demo.drivers_convert_to_delta
+# MAGIC SELECT * FROM f1_demo.drivers_merge
+
+# COMMAND ----------
+
+# MAGIC %sql
+# MAGIC CONVERT TO DELTA f1_demo.drivers_convert_to_delta;
+
+# COMMAND ----------
+
+df = spark.table("f1_demo.drivers_convert_to_delta")
+
+# COMMAND ----------
+
+df.write.format("parquet").save("/mnt/formula1dlmr/demo/drivers_convert_to_delta_new")
+
+# COMMAND ----------
+
+# MAGIC %sql
+# MAGIC CONVERT TO DELTA parquet.`/mnt/formula1dlmr/demo/drivers_convert_to_delta_new`
